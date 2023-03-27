9d494f639ded053fa192e5cf9822c6977972689e
PauloAquino36 <104040796+PauloAquino36@users.noreply.github.com>
2023-03-27 14:33:21 -0300

    upgrade no codigo

--- aula-2/ex4.cpp
+++ aula-2/ex4.cpp
@@ -4,27 +4,23 @@ using namespace std;
 
 bool func(int tam, int vet[], int *par, int *impar, int *negativos)
 {
-    int qPares = 0;
-    int qImpares = 0;
-    int qNegativos = 0;
+    *par = 0;
+    *impar = 0;
+    *negativos = 0;
 
     for(int contaPares = 0; contaPares < tam; contaPares++)
-        if(vet[contaPares] % 2 == 0)
-            qPares++;
-
-    for(int contaImpares = 0; contaImpares < tam; contaImpares++)
-        if(vet[contaImpares] % 2 != 0)
-            qImpares++;
+        {
+            if(vet[contaPares] % 2 == 0)
+                (*par)++;
+            else
+                (*impar)++;
+        }
 
     for(int contaNegativos = 0; contaNegativos < tam; contaNegativos++)
         if(vet[contaNegativos] < 0)
-            qNegativos++;
+            (*negativos)++;
 
-    cout << "numero de pares: " << qPares << endl;
-    cout << "numero de impares: " << qImpares << endl;
-    cout << "numero de negativos: " << qNegativos << endl;
-
-    if(qNegativos > 0)
+    if(*negativos > 0)
         return 1;
     else
         return 0;
@@ -51,14 +47,18 @@ int main()
     for(int c = 0; c < tam; c++)
         cin >> vet[c];
 
-    bool r = func(tam, vet, ptPar, ptImpar, ptNegativos);
+    bool r = func(tam, vet, &par, &impar, &negativos);
+
+    cout << "numero de pares: " << par << endl;
+    cout << "numero de impares: " << impar << endl;
+    cout << "numero de negativos: " << negativos << endl;
 
     if(r == 1)
         cout << "o vetor tem elementos negativos" << endl;
     else
         cout << "o vetor nao tem elementos negativos" << endl;
 
-    delete vet;
+    delete [] vet;
 
     return 0;
 }
