2c3c69ffb8765b9873ac1bfe2adc1b75f14ecddf
PauloAquino36 <104040796+PauloAquino36@users.noreply.github.com>
2023-03-27 15:24:43 -0300

    Create ex2.cpp

---
+++ aula-3/ex2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimeIntervalo(int a, int b, int inc)
+{
+    if(a < b)
+    {
+        cout << a << " ";
+        imprimeIntervalo((a + inc), b, inc);
+    }
+}
+
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int inc = 0;
+
+    cin >> a;
+    cin >> b;
+    cin >> inc;
+
+    imprimeIntervalo(a, b, inc);
+
+    return 0;
+}
