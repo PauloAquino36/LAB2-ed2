0ce6fae5f3c7452748ae0d44093dd80da743fb24
PauloAquino36 <104040796+PauloAquino36@users.noreply.github.com>
2023-05-15 16:02:50 -0300

    Add files via upload

---
+++ aula-7/ListaEncad.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <cstdlib>
+#include "ListaEncad.h"
+
+using namespace std;
+
+ListaEncad::ListaEncad()
+{
+    cout << "Criando objeto ListaEncad" << endl;
+    primeiro = NULL;
+    ultimo = NULL;
+    n = 0;
+}
+
+ListaEncad::~ListaEncad()
+{
+    cout << "Destruindo objeto ListaEncad" << endl;
+    No *p = primeiro;
+    while(p != NULL)
+    {
+        No *t = p->getProx();
+        delete p;
+        p = t;
+    }
+}
+
+bool ListaEncad::busca(int val)
+{
+    No *p;
+    for(p = primeiro; p != NULL; p = p->getProx())
+        if(p->getInfo() == val)
+            return true;
+    return false;
+}
+
+int ListaEncad::get(int k)
+{
+    No *p = primeiro;
+    int i = 0;
+    while(i < k && p != NULL)
+    {
+        i++;
+        p = p->getProx();
+    }
+    if(p == NULL)
+    {
+        cout << "ERRO: Indice invalido!" << endl;
+        exit(1);
+    }
+    else
+        return p->getInfo();
+}
+
+void ListaEncad::set(int k, int val)
+{
+    No *p = primeiro;
+    int i = 0;
+    while(i < k && p != NULL)
+    {
+        i++;
+        p = p->getProx();
+    }
+    if(p == NULL)
+        cout << "ERRO: Indice invalido!" << endl;
+    else
+        p->setInfo(val);
+}
+
+
+void ListaEncad::insereInicio(int val)
+{
+    No* p = new No();
+    p->setInfo(val);
+    p->setProx(primeiro);
+
+    primeiro = p;
+
+    n++;
+    if(n == 1) ultimo = p;
+}
+
+void ListaEncad::insereFinal(int val)
+{
+    No* p = new No();
+    p->setInfo(val);
+    p->setProx(NULL);
+
+    if(ultimo != NULL) ultimo->setProx(p);
+    ultimo = p;
+
+    n++;
+    if(n == 1) primeiro = p;
+}
+
+void ListaEncad::removeInicio()
+{
+    No* p;
+    if(primeiro != NULL)
+    {
+        p = primeiro;
+        primeiro = p->getProx();
+        delete p;
+
+        n--;
+        if(n == 0) ultimo = NULL;
+    }
+    else
+        cout << "ERRO: lista vazia!" << endl;
+}
+
+void ListaEncad::removeFinal()
+{
+    No* p;
+    if(ultimo != NULL)
+    {
+        if(primeiro == ultimo)
+        {
+            primeiro = NULL;
+            p = NULL;
+        }
+        else
+        {
+            p = primeiro;
+            while(p->getProx() != ultimo)
+                p = p->getProx();
+            p->setProx(NULL);
+        }
+        delete ultimo;
+        ultimo = p;
+        n--;
+    }
+    else
+        cout << "ERRO: lista vazia!" << endl;
+}
+
+void ListaEncad::imprime()
+{
+    No *p = primeiro;
+
+    cout << "lista: ";
+    while(p != NULL)
+    {
+            cout << p->getInfo() << " ";
+            p = p->getProx();
+    }
+
+    //cout << "lista: ";
+    //for(No *i = primeiro; i != NULL; i->getProx())
+    //    cout << i->getInfo() << " ";
+    //cout << endl;
+
+    cout << endl;
+}
+
+int ListaEncad::numNos()
+{
+    return n;
+}
+
+int ListaEncad::buscaMaior(int val)
+{
+    int c = 0;
+    for(No *i = primeiro; i != NULL; i = i->getProx())
+    {
+        if(i->getInfo() > val)
+            return c;
+        else
+            c++;
+    }
+    return -1;
+}
+
+void ListaEncad::limpar()
+{
+    No *i = primeiro;
+
+    while(i != NULL)
+    {
+        No *c = i;
+        i = i->getProx();
+        delete c;
+    }
+    primeiro = NULL;
+    ultimo = NULL;
+    n = 0;
+}
+
+float ListaEncad::calculaMedia()
+{
+    if(n==0)
+    {
+        cout << "error, lista vazia!" << endl;
+        return 0;
+    }
+    else
+    {
+       float media = 0;
+        for(No *i = primeiro; i != NULL; i = i->getProx())
+        {
+            media += i->getInfo();
+        }
+        return media/n;
+    }
+
+}
+
+void ListaEncad::concatena(ListaEncad *l2)
+{
+    for(No *i = l2->primeiro; i != NULL; i = i->getProx())
+    {
+      insereFinal(i->getInfo());
+    }
+}
+
+ListaEncad* ListaEncad::partir(int x)
+{
+    int c = 0;
+    ListaEncad *l3;
+
+    for(No *i = primeiro; i != NULL; i = i->getProx())
+    {
+        c++;
+        if(c >= x)
+        {
+            l3->insereFinal(i->getInfo());
+        }
+    }
+    return l3;
+}
---
+++ aula-7/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib> ///rand()
+#include <ctime>   ///time()
+#include "ListaEncad.h"
+
+using namespace std;
+
+int numAleatorio(int a, int b)
+{
+    return a + rand()%(b - a + 1) ; /// retorna um numero inteiro aleatório entre a e b
+}
+
+int main()
+{
+    ListaEncad l; /// cria lista vazia
+    int NumNos = 4;
+
+    srand(time(NULL));
+    /// ATENCAO: os valores inseridos na lista sao gerados
+    /// aleatoriamente e mudam em cada execucao do programa!!!
+    cout << "Inserindo valores: ";
+    for(int i = 1; i <= NumNos; i++)
+    {
+        int val =  numAleatorio(0, 50); /// cria um valor aleatório entre 0 e 50
+        cout << val << ", ";
+        //l.insereInicio(val);
+        l.insereFinal(val);
+    }
+    cout << endl;
+
+    l.imprime();
+
+    cout << "numero de nos: " << l.numNos() << endl;
+
+    cout << "O valor maior que 20 esta na posicao: " << l.buscaMaior(20) << endl;
+    cout << "media: " << l.calculaMedia() << endl;
+
+
+
+    bool existe = l.busca(20);
+    cout << "O valor 20 esta na lista? ";
+    if(existe)
+        cout << "Sim" << endl;
+    else
+        cout << "Nao" << endl;
+
+
+    //questao de coocatenar
+    /*
+    ListaEncad l2; /// cria lista vazia
+
+    srand(time(NULL));
+    /// ATENCAO: os valores inseridos na lista sao gerados
+    /// aleatoriamente e mudam em cada execucao do programa!!!
+    cout << "Inserindo valores: ";
+    for(int i = 1; i <= NumNos; i++)
+    {
+        int val =  numAleatorio(0, 50); /// cria um valor aleatório entre 0 e 50
+        cout << val << ", ";
+        l2.insereInicio(val);
+        //l2.insereFinal(val);
+    }
+    cout << endl;
+
+    l2.imprime();
+    l.concatena(&l2);
+    cout << "lista coocatenadas: ";
+    l.imprime();
+    */
+
+    cout << "limpar lista" << endl;
+    l.limpar();
+    l.imprime();
+    l.calculaMedia();
+
+    return 0;
+};
+
